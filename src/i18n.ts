7ffc0c585041dd4b9375e5a3379e7aedac4c7be6
Gindra-o7 <gilangindra734@gmail.com>
2026-02-28 17:29:04 +0700

    feat: Initialize i18n configuration with language detection and supported translations for English, Indonesian, and Chinese.

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,7 +13,9 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: "id",
+    fallbackLng: "en",
+    supportedLngs: ["id", "en", "zh"],
+    nonExplicitSupportedLngs: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
